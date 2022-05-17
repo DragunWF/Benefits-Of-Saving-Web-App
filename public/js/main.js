f0e7b1d10d1497e5c9f5ba3a3769d64aaad4f649
DragonWF <plarisanmarc@gmail.com>
2022-05-17 21:33:05 +0800

    Minor bug fixes and finished convertToOrdinal() function

--- public/js/main.js
+++ public/js/main.js
@@ -3,25 +3,31 @@ $(document).ready(() => {
   let timesIconClicked = 0;
 
   function convertToOrdinal(number) {
-    const numberString = number.toString();
-    const lastNumber = Number(numberString.charAt(numberString.length - 1));
+    const charArray = number.toString().split("");
+    const lastNumber = charArray[charArray.length - 1];
+    let ordinal;
 
     switch (lastNumber) {
-      case 1:
+      case "1":
+        ordinal = "st";
         break;
-      case 2:
+      case "2":
+        ordinal = "nd";
         break;
-      case 3:
+      case "3":
+        ordinal = "rd";
         break;
       default:
+        ordinal = "th";
         break;
     }
+
+    return `${number}${ordinal}`;
   }
 
-  icons.clicked(() => {
+  icons.click(() => {
     timesIconClicked++;
-    const timesWord = timesIconClicked > 1 ? "time" : "times";
     const formattedCount = convertToOrdinal(timesIconClicked);
-    alert(`This is the ${formattedCount} ${timesWord}`);
+    alert(`This is the ${formattedCount} time you clicked an icon.`);
   });
 });
