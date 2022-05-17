a81382b2b9887ffeefd137432ac0230409d109c8
DragonWF <plarisanmarc@gmail.com>
2022-05-18 01:42:26 +0800

    Refactored for deployment to heroku

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
+import "dotenv/config";
 import express from "express";
-import serverless from "serverless-http";
-import VisitTracker from "../utils/visits-tracker";
+import VisitTracker from "./utils/visits-tracker.js";
 
 const app = express();
-const router = express.Router();
+const port = process.env.PORT || 8888;
 
 app.set("view engine", "ejs");
 app.use("/public", express.static("public"));
 
-router.get("/", (req, res) => {
+app.get("/", (req, res) => {
   VisitTracker.incrementVisits();
   res.render("index", {
     data: {
@@ -17,6 +17,6 @@ router.get("/", (req, res) => {
   });
 });
 
-app.use("/", router);
-
-export const handler = serverless(app);
+app.listen(port, () => {
+  console.log("Server is now running!");
+});
