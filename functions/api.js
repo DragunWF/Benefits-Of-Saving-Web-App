8345c61e233deb9ebabdee4d10f4046519e537e6
DragonWF <plarisanmarc@gmail.com>
2022-05-17 20:33:51 +0800

    Set up ejs

--- functions/api.js
+++ functions/api.js
@@ -4,8 +4,11 @@ import serverless from "serverless-http";
 const app = express();
 const router = express.Router();
 
+app.set("view engine", "ejs");
+app.use("/public", express.static("public"));
+
 router.get("/", (req, res) => {
-  res.send("Testing");
+  res.render("index");
 });
 
 app.use("/", router);
